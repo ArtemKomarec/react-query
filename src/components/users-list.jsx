d5a5b003c3d3827c71a1699bdaa0bd9cd3811210
ArtemKomarec <artemkomarec10@gmail.com>
2023-01-12 12:27:54 +0300

    feat: add user search

--- src/components/users-list.jsx
+++ src/components/users-list.jsx
@@ -1,24 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import { USERS_API } from "../constants";
 import { UserProfile } from "./user-profile";
 import { UsersSearch } from "./users-search";
 
 export const UsersList = () => {
+	const [search, setSearch] = useState("")
+	const queryInfo = useQuery({
+    queryKey: ["searchUsers", search],
+    queryFn: async () => {
+      const result = await axios
+        .get(`${USERS_API}/${search}`, { queryParams: { query: search } })
+		console.log(result)
+		return result
+    },
+    enabled: Boolean(search),
+  });
 	return (
-		<div>
-			<UsersSearch />
-			<div
-				style={{
-					marginTop: 40,
-					display: "flex",
-					justifyContent: "center",
-					gap: 40,
-				}}
-			>
-				<UserProfile />
-				<UserProfile />
-				<UserProfile />
-				<UserProfile />
-				<UserProfile />
-			</div>
-		</div>
-	);
+    <div>
+      <UsersSearch search={search} setSearch={setSearch} />
+      <div
+        style={{
+          marginTop: 40,
+          display: "flex",
+          justifyContent: "center",
+          gap: 40,
+        }}
+      ></div>
+    </div>
+  );
 };
--- src/components/users-search.jsx
+++ src/components/users-search.jsx
@@ -1,10 +1,13 @@
-export const UsersSearch = () => {
+export const UsersSearch = ({search, setSearch}) => {
+	const handleChange = (e) => {
+		setSearch(e.target.value)
+	}
 	return (
 		<div
 			style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
 		>
 			<h1>Search users</h1>
-			<input type="text" />
+			<input type="text" onChange={handleChange}/>
 		</div>
 	);
 };
