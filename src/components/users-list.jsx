f329ff2c75a51f167a772736fb85bb6c20a3f74c
ArtemKomarec <artemkomarec10@gmail.com>
2023-01-12 15:04:48 +0300

    feat: add tailwind
    feat: change api
    feat: add userslist

--- src/components/users-list.jsx
+++ src/components/users-list.jsx
@@ -2,32 +2,45 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useState } from "react";
 import { USERS_API } from "../constants";
-import { UserProfile } from "./user-profile";
 import { UsersSearch } from "./users-search";
+import React from "react";
+import useDebounce from "./debounce";
 
 export const UsersList = () => {
 	const [search, setSearch] = useState("")
-	const queryInfo = useQuery({
-    queryKey: ["searchUsers", search],
+	const debouncedSearchValue = useDebounce(search, 500)
+	const {
+		isLoading,
+		error,
+		data
+	} = useQuery({
+    queryKey: ["users", debouncedSearchValue],
     queryFn: async () => {
       const result = await axios
-        .get(`${USERS_API}/${search}`, { queryParams: { query: search } })
-		console.log(result)
-		return result
+        .get(`${USERS_API + search}`)
+		return result;
     },
     enabled: Boolean(search),
   });
+
+ 	// if (isLoading) return <p>Loading</p>;
+
+	// if (error) return <p>Error - ${error.message}</p>;
+
 	return (
     <div>
       <UsersSearch search={search} setSearch={setSearch} />
-      <div
-        style={{
-          marginTop: 40,
-          display: "flex",
-          justifyContent: "center",
-          gap: 40,
-        }}
-      ></div>
+      {isLoading && <p>Loading</p>}
+      {error && <p>Error - {error.message}</p>}
+	  {data && (
+		<div className="grid grid-cols-4" >
+        {data.data.items.map((user) => (
+          <div className="my-[12px]" key={user.id}>
+            <p>{user.login}</p>
+          </div>
+        ))}
+      </div>
+	  )}
     </div>
   );
 };
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const USERS_API = "https://api.github.com/users";
+export const USERS_API = "https://api.github.com/search/users?q=";
 export const USERS_REPOSITORY_API =
 	"https://api.github.com/repos/USERNAME/PROJECT";
